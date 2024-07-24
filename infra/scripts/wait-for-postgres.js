5dc97bf8e0cc49d145c9fe8d061382a4c0820d52
Igor Augusto <igor.a.s.silva@gmail.com>
2024-07-24 09:59:06 -0300

    fix `prettier` style linting`

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,7 +1,7 @@
 const { exec } = require("node:child_process");
 
 function checkPostgres() {
-  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn)
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
   function handleReturn(error, stdout) {
     if (stdout.search("accepting connections") === -1) {
@@ -10,10 +10,9 @@ function checkPostgres() {
       return;
     }
 
-    console.log("\n🟢 Postgres está pronto e aceitando conexões!\n")
+    console.log("\n🟢 Postgres está pronto e aceitando conexões!\n");
   }
 }
 
 process.stdout.write("\n\n🔴 Aguardando Postgres aceitar conexões");
 checkPostgres();
-
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,14 +2,14 @@ import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-})
+});
 
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-  
+
   const responseBody = await response.json();
-  
+
   const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 
